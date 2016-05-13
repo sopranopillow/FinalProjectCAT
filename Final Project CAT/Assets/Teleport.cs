9d06db6461a83d5d29f9df2a24866c613bd96fb4
DavidDSS <daviddss1999@hotmail.com>
2016-05-13 09:09:17 -0500

    Change 2
    Scrolling and teletransportation confirmar

---
+++ Final Project CAT/Assets/Teleport.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	//Set up a variable to access the player from
+	private Transform player; 
+
+	void Awake()
+	{
+		//Find the player object and set it
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+	void Update()
+	{
+		// Check if you click the left mouse button then change position
+		if (Input.GetMouseButtonDown(0))
+			player.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+}
