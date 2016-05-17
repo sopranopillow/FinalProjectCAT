b16a58ccd7c184efb2b897929b0e4a0aebfd1571
DavidDSS <daviddss1999@hotmail.com>
2016-05-17 14:22:33 -0500

    Change #26
    - Created a simpler Ball teleportation script

---
+++ Final Project CAT/Assets/Scripts/BallTeleportSimple.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallTeleportSimple : MonoBehaviour {
+
+	Vector3 newPosition;
+
+
+	void Start(){
+		newPosition = transform.position;
+	}
+
+	void Update(){
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit)) {
+				newPosition = new Vector3(hit.point.x, hit.point.y, -0.5f);
+				transform.position = newPosition;
+			}
+		}
+	}
+
+}
